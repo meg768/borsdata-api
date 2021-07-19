6b145e39f614f17a91aa083f6732c76c2488ffea
meg768 <meg768@users.noreply.github.com>
2021-07-19 19:11:18 +0200

    -

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 #!/usr/bin/env node
 
-console.log("HEJ");
+var Request = require('yow/request');
+
+
+require('dotenv').config();
+
+class BorsdataAPI {
+
+	constructor() {
+		var defaultOptions = {
+			debug: true,
+			headers: {
+				'content-type': 'application/json'
+			}
+		};
+
+		this.rootUrl = `https://apiservice.borsdata.se/v1`;
+		this.apiKey = process.env.API_KEY;
+		this.request = new Request(this.rootUrl, defaultOptions);
+	}
+
+	async get(path) {
+		var query = {
+			authKey:this.apiKey
+		}
+		return this.request.get(path, {query:query});
+		return result;
+
+	}
+	async getMarkets() {
+		return this.get('branches');
+	}
+}
+
+async function run() {
+
+	try {
+		let api = new BorsdataAPI();
+		let markets = await api.getMarkets();
+		console.log(JSON.stringify(markets, null, "   "));
+	
+	}
+	catch(error) {
+		console.log(error);
+	}	
+}
+
+run();
